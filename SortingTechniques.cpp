2e0b42d3702f1596f2bf6908223ace857d69dfa0
vasundhara bhati <73017117+vasundhara2405@users.noreply.github.com>
2021-05-18 16:05:31 +0530

    Update SortingTechniques.cpp

--- SortingTechniques.cpp
+++ SortingTechniques.cpp
@@ -1,6 +1,7 @@
-
-#include <bits/stdc++.h>
+#include  <bits/stdc++.h>
 using namespace std;
+int c=0,t=0,k=0,r=0 ;
+//insertion sort with comparision
 void insertionSort(int arr[], int n) {
   int i, key ,j, count=0;
   for (i = 1; i < n; i++) {
@@ -17,93 +18,65 @@ count++;
   }
   cout<<"Comparisons : "<<count<<endl;
 }
-void swap(int* a, int* b)
+void printArray(int arr[], int n)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
-int partition (int arr[], int low, int high)
+//merge sort with comparision
+void swap(int *x,int *y)
 {
-    int pivot = arr[high]; 
-    int i = (low - 1); 
- 
-    for (int j = low; j <= high - 1; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            i++; 
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+int temp=*x;
+*x=*y;
+*y=temp;
+}
+void Merge(int A[],int l,int mid,int h)
+{
+int i=l,j=mid+1,k=l;
+int B[100];
+
+while(i<=mid && j<=h)
+{
+if(A[i]<A[j])
+B[k++]=A[i++];
+else
+B[k++]=A[j++];
+c++;
 }
 
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
+for(;i<=mid;i++)
+B[k++]=A[i];
+for(;j<=h;j++)
+B[k++]=A[j];
+for(i=l;i<=h;i++)
+A[i]=B[i];
 }
-void merge(int arr[], int l, int m, int r)
+void MergeSort(int A[],int l,int h)
 {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int L[n1], R[n2];
-  for (int i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
-  int i = 0;
-   int j = 0;
- int k = l;
+int mid;
+if(l<h)
+{
+
+mid=(l+h)/2;MergeSort(A,l,mid);
+MergeSort(A,mid+1,h);
+Merge(A,l,mid,h);
+}
+
+}
+void comparsion(){
+cout<<"comparsion  :"<<c<<endl;
+}
+// bubble sort
+void Swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
  
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        }
-        else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-   while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-  while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-}
-void mergeSort(int arr[],int l,int r){
-    if(l>=r){
-        return;
-    }
-    int m =l+ (r-l)/2;
-    mergeSort(arr,l,m);
-    mergeSort(arr,m+1,r);
-    merge(arr,l,m,r);
-}
-void selectionSort(int arr[], int n) 
-{ 
-    int i, j, min_idx; 
-    for (i = 0; i < n-1; i++) 
-    {   min_idx = i; 
-        for (j = i+1; j < n; j++) 
-        if (arr[j] < arr[min_idx]) 
-            min_idx = j; 
-   swap(&arr[min_idx], &arr[i]); 
-    } 
-}
+// A function to implement bubble sort
 void bubbleSort(int arr[], int n)
 {
     int i, j;
@@ -112,34 +85,154 @@ void bubbleSort(int arr[], int n)
     // Last i elements are already in place
     for (j = 0; j < n-i-1; j++)
         if (arr[j] > arr[j+1])
-            swap(&arr[j], &arr[j+1]);
+        t++;
+            Swap(&arr[j], &arr[j+1]);
 }
-void printArray(int arr[], int size)
+ 
+  void compare(){
+  cout<<"comparision:-"<<  t<<endl;
+ }
+/* Function to print an array */
+void printarray(int arr[], int size)
 {
     int i;
     for (i = 0; i < size; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
-int main()
+// selection sort
+void swapp(int *xp, int *yp)
 {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+ 
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+        if (arr[j] < arr[min_idx])
+        k++;
+            min_idx = j;
+ 
+        // Swap the found minimum element with the first element
+        swapp(&arr[min_idx], &arr[i]);
+    }
+}
+  void comp(){
+  cout<<"no.of comparsion:-"<<k<<endl;
+   }
+/* Function to print an array */
+void printArrayy(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+//Quick sort
+void swaap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];  
+    int i = (low - 1);  
+ 
+    for (int j = low; j <= high- 1; j++)
+    {
+        if (arr[j] <= pivot)
+        { r++;
+            i++;    
+            swaap(&arr[i], &arr[j]);
+        }
+    }
+    swaap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+ 
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    { int pi = partition(arr, low, high);
+  quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+void cmp(){
+cout<<"no. of comparsion:--"<<r<<endl;
+}
+
+void printtArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+       
+        cout<<arr[i]<<"\n";
+   
+}
+ 
+int main()
+ { //insertion sort
+ cout<<"insertion sort:--"<<endl;
+ 
   int arr[]={5,4,10,1,6,2};
   int n=sizeof(arr)/sizeof(arr[0]);
-   
+
    insertionSort(arr,n);
-    cout<<"Sorted array after Insertion sort:\n";
-   printArray(arr,n);
-   cout<<"Sorted array after Quick sort:\n";
-   quickSort(arr, 0, n - 1);
-    printArray(arr,n);
-   cout<<"Sorted array after Merge sort:\n";
-    mergeSort(arr, 0, n - 1);
-   printArray(arr,n);
-   cout<<"Sorted array after Selection sort:\n";
-   selectionSort(arr, n); 
-   printArray(arr,n);
- cout<<"Sorted array after Bubble sort:\n";
-   bubbleSort(arr, n); 
-   printArray(arr,n);
+  printArray(arr,n);
+  cout<<"-----------------------------"<<endl;
+ 
+  //merge sort
+  cout<<"merge sort :--"<<endl;
+  int A[]={11,13,7,12,16,9,24,5,10,3},b=10,i;
+MergeSort(A,0,b-1);
+comparsion();
+cout<<"Array after Sorting"<<endl;
+for(i=0;i<10;i++)
+cout<<A[i]<<endl;
+cout<<"-----------------------------"<<endl;
+
+
+// bubble sort
+cout<<"bubble sort :--"<<endl;
+int aryr[] = {64, 34, 25, 12, 22, 11, 90};
+    int h = sizeof(aryr)/sizeof(aryr[0]);
+    bubbleSort(aryr, h);
+    compare();
+    cout<<"Sorted array: \n";
+    printarray(aryr, h);
+    cout<<"-----------------------------"<<endl;
+   
+    // selection sort
+    cout<<"selection sort :--"<<endl;
+    int arri[] = {64, 25, 12, 22, 11};
+    int a = sizeof(arri)/sizeof(arri[0]);
+    selectionSort(arri, a);
+    comp();
+    cout << "Sorted array: \n";
+    printArrayy(arri, a);
+    cout<<"-----------------------------"<<endl;
+   
+//quick sort
+cout<<"Quick sort :--"<<endl;
+int aarr[] = {10, 7, 8, 9, 1, 5};
+    int p = sizeof(aarr)/sizeof(aarr[0]);
+    quickSort(aarr, 0, p-1);
+    cmp();
+    cout<<"sorted array";
+    printtArray(aarr, p);
+
   return 0;
 }
